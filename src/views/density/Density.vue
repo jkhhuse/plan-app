bdd34ffbcc3ce3913ae7339e3c7c6f5f8957ddc8
jiangkang <jkhhuse@126.com>
2021-11-09 23:52:45 +0800

    尝试增加 f2 作为chart lib

--- src/views/density/Density.vue
+++ src/views/density/Density.vue
@@ -13,7 +13,12 @@
   </div>
   <div class="text-left m-2 mt-4 flex flex-row justify-between">
     <div class="bg-white h-32 w-1/3 mr-2">
-      <div>标题</div>
+      <div class="mobile-container">
+        <div class="mobile-header"></div>
+        <div class="mobile-content">
+          <canvas ref="container" />
+        </div>
+      </div>
     </div>
     <div class="bg-white h-32 flex-grow">
       <div>标题</div>
@@ -22,71 +27,45 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "@vue/runtime-core";
-import { Toast } from "vant";
-import { Profile } from "@/types/profile";
-import { areaList } from "@vant/area-data";
-import { HttpMessage, VantAreaType } from "@/types/index";
-import * as crypto from "crypto-js";
-import { getProfile } from "@/action/profile/profile";
+import { defineComponent, ref, onMounted } from "@vue/runtime-core";
+import F2 from "@antv/f2";
 
 export default defineComponent({
   setup() {
-    const profile = ref<Profile>({
-      name: "",
-      bornTime: "",
-      origin: 0,
-      createTime: "",
-      addr: "",
-      email: "",
-      passwd: "",
-    });
+    // const ctx = ref<any>(null);const $axios = inject('$axios');
 
-    const showBornTime = ref<boolean>(false);
-    const showAddr = ref<boolean>(false);
+    // const ctx = document.getElementById("myChart") as any;
 
-    const onBornTimeConfirm = (date: Date) => {
-      showBornTime.value = false;
-      profile.value.bornTime = `${date.getFullYear() + 1}/${date.getMonth() + 1}/${date.getDate()}`;
-    };
+    // F2 对数据源格式的要求，仅仅是 JSON 数组，数组的每个元素是一个标准 JSON 对象。
 
-    const onAddrConfirm = (values: VantAreaType[]) => {
-      showAddr.value = false;
-      profile.value.addr = values
-        .filter((item: VantAreaType) => !!item)
-        .map((item: VantAreaType) => item.name)
-        .join("/");
-    };
-
-    const onClickLeft = () => Toast("返回");
-
-    const onClickRight = () => Toast("按钮");
-
-    const updateProfile = () => {
-      crypto.DES.decrypt(profile.value.passwd, "plan app").toString();
-    };
+    const container = ref<any>(null);
 
     onMounted(() => {
-      getProfile().subscribe((res: HttpMessage<Profile>) => {
-        profile.value = res.data;
-        console.log(res.data);
+      const data = [
+        { genre: "Sports", sold: 275 },
+        { genre: "Strategy", sold: 115 },
+        { genre: "Action", sold: 120 },
+        { genre: "Shooter", sold: 350 },
+        { genre: "Other", sold: 150 },
+      ];
+      // Step 1: 创建 Chart 对象
+      const chart = new F2.Chart({
+        id: container.value,
+        pixelRatio: window.devicePixelRatio, // 指定分辨率
       });
+
+      // Step 2: 载入数据源
+      chart.source(data);
+
+      // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
+      chart.interval().position("genre*sold").color("genre");
+
+      // Step 4: 渲染图表
+      chart.render();
     });
 
-    // const saveProfile = () => {
-    // }
-
     return {
-      profile,
-      showBornTime,
-      showAddr,
-      areaList,
-      onBornTimeConfirm,
-      onAddrConfirm,
-      onClickLeft,
-      onClickRight,
-      updateProfile,
-      // saveProfile
+      container,
     };
   },
 });
