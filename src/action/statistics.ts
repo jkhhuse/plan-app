3828f5995ce672e70c193854ea60a732c8391263
jkhhuse <jkhhuse@126.com>
2022-01-04 18:56:30 +0800

    fix

---
+++ src/action/statistics.ts
@@ -0,0 +1,28 @@
+import { HttpMessage } from "@/types/index";
+import { Observable } from "rxjs";
+import { catchError } from "rxjs/operators";
+import { http } from "@/action";
+import { PLAN_SERVER } from "@/constants/common";
+import { retryBackoff } from "backoff-rxjs";
+import { handleError } from "@/action/common/handle";
+import { Statisitcs } from "@/types/statistics";
+
+/**
+ * 搜索最近 N 天的数据
+ * @param measureTime 测量时间
+ * @returns
+ */
+export const searchFoodAction = (measureTime: string, days: string): Observable<HttpMessage<Statisitcs>> => {
+  const url = PLAN_SERVER + "stats/latest/";
+  const payload = {
+    days: days,
+    measureTime: measureTime,
+  };
+  return http.post<HttpMessage<Statisitcs>>(url, payload).pipe(
+    retryBackoff({
+      maxRetries: 1,
+      initialInterval: 4000,
+    }),
+    catchError(handleError),
+  );
+};
---
+++ src/types/statistics.ts
@@ -0,0 +1,7 @@
+export interface Statisitcs {
+  natureProtein: number;
+  pheValue: number;
+  specialProtein: number;
+  specialProteinMl: number;
+  totalProtein: number;
+}
